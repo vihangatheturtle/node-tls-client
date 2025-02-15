ed75f4b4c20687b7b84388c5a05fdb15654b8b1a
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2025-02-15 16:10:04 +0000

    Update download.js

--- dist/utils/download.js
+++ dist/utils/download.js
@@ -66,8 +66,8 @@ class Download {
         const percentage = (downloaded / total) * 100;
         const progress = Math.floor(percentage / 2);
         const bar = "█".repeat(progress) + " ".repeat(50 - progress);
-        process.stdout.clearLine(0);
-        process.stdout.cursorTo(0);
+        // process.stdout.clearLine(0);
+        // process.stdout.cursorTo(0);
         process.stdout.write(`${logger_1.logger.stamp} DOWNLOADING:[${bar}] ${percentage.toFixed(2)}% (${this.formatBytes(downloaded)} / ${this.formatBytes(total)})`);
     }
     async download(url, file) {
